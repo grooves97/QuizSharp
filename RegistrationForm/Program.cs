804c006cc81cef61163787a6c6e69c9e8c915f02
Аслан Нурсеитов <grooves97@gmail.com>
2019-03-07 14:17:45 +0600

    Quiizzzz

---
+++ RegistrationForm/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+
+namespace RegistrationForm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            users.Add(new User() { Email="grooves97@gmail.com", Password="123",City="Astana",Phone=8777,Age=10});
+            users.Add(new User() { Email="serik@gmail.com",Password="123",City="Astana",Phone=7788,Age=30});
+
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(List<User>));
+
+            using (FileStream fs = new FileStream("./people.json", FileMode.OpenOrCreate))
+            {
+                jsonFormatter.WriteObject(fs, users);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegistrationForm/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+
+namespace RegistrationForm
+{
+    [DataContract]
+    class User
+    {
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public long Phone { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+        [DataMember]
+        public string City { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+
+        public User()
+        {
+            Email = string.Empty;
+            Password = string.Empty;
+            Phone = 0;
+            City = string.Empty;
+            Age = 0;
+        }
+
+
+    }
+}
